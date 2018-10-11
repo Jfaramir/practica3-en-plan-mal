07ff7ccb9ea55379d0054d942df141582dc3215b
xp <xp@CETYSDAM028>
2018-10-11 20:23:27 +0200

    qwrawesffvdaasd

---
+++ src/DOM.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xp
+ */
+public class DOM {
+
+    public DOM() {
+    }
+
+    public int abrir_XML_DOM() {
+        
+    }
+    
+}
